b4324eb5e78589aa02678e6947efa14b9c6d818c
Prateek Sujaina <prateek.sujaina@gmail.com>
2024-04-23 13:38:45 +0530

    Initial Commit

---
+++ CurrencyConvertor/CurrencyConvertorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConvertorApp.swift
+//  CurrencyConvertor
+//
+//  Created by Prateek Sujaina on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConvertorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
